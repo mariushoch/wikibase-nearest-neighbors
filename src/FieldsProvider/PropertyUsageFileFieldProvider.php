936cff0cec4c78ef863e6e3d75ee6f00e09a2712
Marius Hoch <hoo@online.de>
2017-12-30 20:15:43 +0100

    Add PropertyUsageFileFieldProvider

---
+++ src/FieldsProvider/PropertyUsageFileFieldProvider.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Wikibase\NearestNeighbors\FieldProviders;
+use RuntimeException;
+
+/**
+ * Get a field mapping from a given PropertyUsageFile (as obtained from getPropertyUsages).
+ */
+class PropertyUsageFileFieldProvider implements FieldProvider {
+
+	/**
+	 * @var string
+	 */
+	private $fileName;
+
+	/**
+	 * @var int[]|null
+	 */
+	private $fields = null;
+
+	/**
+	 * @param string $fileName
+	 */
+	public function __construct( $fileName ) {
+		$this->fileName = $fileName;
+	}
+
+	/**
+	 * @return int[] Guaranteed to be sorted by number of usages.
+	 */
+	public function getFields() {
+		if ( !$this->fields ) {
+			$this->readFromFile();
+		}
+
+		return $this->fields;
+	}
+
+	private function readFromFile() {
+		$f = fopen( $this->fileName, 'r' );
+		$propertyIds = [];
+
+		// Skip the first (header) line
+		fgets( $f );
+
+		$lineNumber = 1;
+		while ( ( $line = fgets( $f ) ) !== false ) {
+			$lineNumber++;
+
+			if ( strlen( $line ) < 2 ) {
+				continue;
+			}
+
+			$id = explode( "\t", $line, 2 )[1];
+			$id = trim( $id );
+			if ( !preg_match( '/^P[1-9]\d{0,9}\z/i', $id ) ) {
+				throw new RuntimeException( "PropertyUsageFileFieldProvider: Invalid line $lineNumber." );
+			}
+
+			$propertyIds[] = $id;
+		}
+
+		$this->fields = array_map( function( $propertyIdSerialization ) {
+			return (int) substr( $propertyIdSerialization, 1 );
+		}, $propertyIds );
+	}
+
+}

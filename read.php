01b0bb0dc7d882091977454a315725a1ffc68e13
Marius Hoch <hoo@online.de>
2017-12-30 03:45:53 +0100

    Adopt read.php for Wikidata

--- read.php
+++ read.php
@@ -2,16 +2,44 @@
 
 namespace Wikibase\NearestNeighbors;
 
+use Wikibase\NearestNeighbors\PropertyIdEncoder;
+use Wikibase\NearestNeighbors\FieldProviders\PropLinksFieldProvider;
+use Wikibase\NearestNeighbors\FieldProviders\WikibaseAllPropertiesFieldProvider;
+
 require_once __DIR__ . '/vendor/autoload.php';
 
-$entityReader = new EntityReader();
+// FIXME: Use getopt or something similar…
+// TODO: Make sure this is not Wikidata specific
+if ( $argc !== 4 || $argc[1] === '--help' || $argc[1] === '-h' ) {
+	echo "read.php: Read a Wikidata JSON dump and output minimal encoding of the statements present.\n\n";
+	echo "Usage: InputFile OutputFileFullEncoding OutputFileTop100Encoding\n";
 
-if ( $argc != 3 ) {
-	die( "This script takes two arguments: Input file and output file!\n" );
+	exit( 1 );
 }
 
+$entityReader = new EntityReader();
+$encoders = [
+	new PropertyIdEncoder(
+		( new WikibaseAllPropertiesFieldProvider(
+			'https://www.wikidata.org/w/api.php',
+			120
+		) )->getFields(),
+		'full'
+	),
+	new PropertyIdEncoder(
+		( new PropLinksFieldProvider(
+			'https://www.wikidata.org/w/api.php',
+			'Wikidata:Database reports/List of properties/Top100',
+			120
+		) )->getFields(),
+		'top100'
+	)
+];
+
+$partitioningPropertyIdEncoder = new PartitioningPropertyIdEncoder( $encoders );
+
 $f = fopen( $argv[1], 'r' );
-$buffer = '';
+$buffer = [];
 $i = 0;
 while ( ( $line = fgets( $f ) ) !== false ) {
 	$i++;
@@ -21,6 +49,24 @@ while ( ( $line = fgets( $f ) ) !== false ) {
 		continue;
 	}
 
-	$numericPropertyIds = $entityReader->readLineString( $line );
-	
+	$entity = $entityReader->readLineString( $line );
+	$encoded = $partitioningPropertyIdEncoder->getEncoded( $entity[1] );
+
+	if ( !isset( $buffer[$encoded[0]] ) || !is_string( $buffer[$encoded[0]] ) ) {
+		$buffer[$encoded[0]] = '';
+	}
+
+	$buffer[$encoded[0]] .= $entity[0] . ':' . $encoded[1] . "\n";
+
+	// Only write to target file for every 10000 rows
+	if ( $i % 10000 === 0 ) {
+		file_put_contents( $argv[2], $buffer['full'], FILE_APPEND );
+		file_put_contents( $argv[3], $buffer['top100'], FILE_APPEND );
+		$buffer = [];
+
+		echo $i . " done\n";
+	}
 }
+
+file_put_contents( $argv[2], $buffer['full'], FILE_APPEND );
+file_put_contents( $argv[3], $buffer['top100'], FILE_APPEND );

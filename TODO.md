c65fc133c7df78f6399e4267a9b73fe09b554190
Marius Hoch <hoo@online.de>
2017-12-30 15:03:28 +0100

    Add basic findNearestNeighbors.php

--- TODO.md
+++ TODO.md
@@ -1,2 +1,4 @@
 * Make `generateEncodingFromDump.php` not Wikidata specific
+* Allow arbitrary file inputs to `findNearestNeighbors.php`
+* Refactor and tests for `generateEncodingFromDump.php` and `findNearestNeighbors.php`
 * (Maybe) Mock production `file_get_contents` calls
---
+++ findNearestNeighbors.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace Wikibase\NearestNeighbors;
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+function getResFromFile( $fileName, $needleEntityData ) {
+	$f = fopen( $fileName, 'r' );
+	$header = fgets( $f );
+
+	if ( !$header ) {
+		echo "File \"$fileName\" must not be empty.\n";
+		exit( 1 );
+	}
+
+	$hammingDistanceCalculator = new IntArrayHammingDistanceCalculator();
+	
+	$entityOrder = array_map( 'intval', explode( ',', $header ) );
+	// Some property ids might not be present in the current encoding file at all,
+	// thus we always have at least this distance.
+	$missingFromList = count( array_diff( $needleEntityData[1], $entityOrder ) );
+
+	$propertyIdEncoder = new PropertyIdEncoder( $entityOrder, 'needleEncoder' );
+	$needle = $propertyIdEncoder->getEncoded( $needleEntityData[1] )[1];
+
+	$needleChunkInts = $propertyIdEncoder->encodingToIntArray( $needle );
+	$needleChunkCount = count( $needleChunkInts );
+
+	$processedEntities = 0;
+	$lineNumber = 1;
+	$cutOff = PHP_INT_MAX;
+	$results = [];
+
+	while ( ( $line = fgets( $f ) ) !== false ) {
+		$processedEntities++;
+		$lineNumber++;
+
+		$colonPos = strpos( $line, ':' );
+		$entityId = substr( $line, 0, $colonPos );
+		$line = substr( $line, $colonPos + 1 );
+
+		$entityData = $propertyIdEncoder->encodingToIntArray( $line );
+		// The byte strings might also contain new lines, thus read more lines if needed.
+		while ( $needleChunkCount > count( $entityData ) ) {
+			$line .= fgets( $f );
+			$lineNumber++;
+			$entityData = $propertyIdEncoder->encodingToIntArray( $line );
+		}
+
+		if ( $needleChunkCount !== count( $entityData ) ) {
+			die( "Found invalid data on line $lineNumber\n" );
+		}
+
+		$dist = $hammingDistanceCalculator->getDistance( $entityData, $needleChunkInts ) + $missingFromList;
+		if ( $dist <= $cutOff ) {
+			$results[$entityId] = $dist;
+		}
+
+		$cutOff = cutOffResults( $results );
+	}
+
+	return $results;
+}
+
+function cutOffResults( &$results ) {
+	if ( count( $results ) <= 50 ) {
+		return PHP_INT_MAX;
+	}
+
+	$maxValue = 0;
+	$maxId = '';
+	foreach ( $results as $id => $result ) {
+		if ( $result > $maxValue ) {
+			$maxValue = $result;
+			$maxId = $id;
+		}
+	}
+
+	unset( $results[$maxId] );
+	return $maxValue;
+}
+
+// FIXME: Use getopt or something similar…
+// TODO: Make sure this is not Wikidata specific
+if ( $argc < 4 || $argc[1] === '--help' || $argc[1] === '-h' ) {
+	echo "generateEncodingFromDump.php: Read a Wikidata JSON dump and output minimal encoding of the statements present.\n\n";
+	echo "Usage: EntityId InputFileFullEncoding InputFileTop100Encoding [minDistance]\n";
+
+	exit( 1 );
+}
+$entityReader = new EntityReader();
+
+$needleEntity = file_get_contents( 'https://www.wikidata.org/wiki/Special:EntityData/' . $argv[1] . '.json' );
+$needleEntityData = $entityReader->readEntityDataString( $needleEntity );
+
+$results = getResFromFile( $argv[2], $needleEntityData );
+$results = array_merge( $results, getResFromFile( $argv[3], $needleEntityData ) );
+
+$displayResults = [];
+$i = 0;
+foreach ( $results as $id => $row ) {
+	$displayResults[$row * 10000 + ++$i] = [ $id, $row ];
+}
+
+ksort( $displayResults );
+$displayResults = array_splice( $displayResults, 0, 50 );
+
+echo "Id\tDistance\n";
+foreach( $displayResults as $res ) {
+	echo "$res[0]\t$res[1]\n";
+}
